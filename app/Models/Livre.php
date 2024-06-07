e39e3e1846dc54edbd3a602ea55f4adcdab3c0dc
DialloMaria <diallomariam0715@gmail.com>
2024-06-07 01:45:51 +0000

    Mise en place des relation  entres les tables

--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Livre extends Model
 {
     use HasFactory;
+
+    public function categorie(){
+        return $this->belongsTo(Categorie::class, 'categorie_id');
+    }
+
+    public function rayon(){
+        return $this->belongsTo(Rayon::class,'rayon_id');
+    }
 }
--- app/Models/Rayon.php
+++ app/Models/Rayon.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rayon extends Model
 {
     use HasFactory;
+
+    public function livre(){
+        return $this->hasMany(Livre::class);
+    }
 }
